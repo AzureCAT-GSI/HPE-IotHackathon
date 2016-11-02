c5b5b9edf50dab08aaddde0124f8b2fabf1753e5
KimmoForss <kimforss@microsoft.com>
2016-11-02 17:29:14 +0200

    Android

---
+++ AndroidApp/MainActivity.cs
@@ -0,0 +1,42 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Util;
+using Gcm.Client;
+
+namespace AndroidApp
+{
+    [Activity(Label = "AndroidApp", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+
+        public static MainActivity instance;
+        protected override void OnCreate(Bundle bundle)
+        {
+            instance = this;
+
+            base.OnCreate(bundle);
+
+            // Set your view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // Get your button from the layout resource,
+            // and attach an event to it
+            Button button = FindViewById<Button>(Resource.Id.button1);
+
+            RegisterWithGCM();
+        }
+
+        private void RegisterWithGCM()
+        {
+            // Check to ensure everything's set up right
+            GcmClient.CheckDevice(this);
+            GcmClient.CheckManifest(this);
+
+            // Register for push notifications
+            Log.Info("MainActivity", "Registering...");
+            GcmClient.Register(this, Constants.SenderID);
+        }
+    }
+}
+
