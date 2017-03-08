6f937f5fea6281f021bf15b09b1ee79b126d3130
KimmoForss <kimforss@microsoft.com>
2017-03-08 09:55:13 +0200

    Added assembly properties

--- AppNotificationDemo/Properties/AssemblyInfo.cs
+++ AppNotificationDemo/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("KFAppNotificationDemo")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("GSIAppNotificationDemo")]
+[assembly: AssemblyDescription("Notification demo app")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("KFAppNotificationDemo")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("GSIAppNotificationDemo")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -24,6 +24,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
 [assembly: ComVisible(false)]
